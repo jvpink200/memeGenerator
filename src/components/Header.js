1270ebacae32b6ce28e77172e70786a184dbf068
jvpink200 <jvillarr20@gmail.com>
2021-08-23 19:10:52 -0700

    added Header and Meme section components

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <React.Fragment>
+        <header>
+          <img
+            src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+            alt="Problem?"
+          />
+          <p>MEME Generator Section</p>
+        </header>
+      </React.Fragment>
+    );
+}
+
+export default Header;
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MemeGenerator extends Component{
+    constructor(){
+        super()
+        this.state={}
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <section>Meme Gen Section</section>
+            </React.Fragment>
+        )
+    }
+}
+
+export default MemeGenerator;
